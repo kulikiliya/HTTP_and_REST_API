1ceb37d11f98c885c873eb56e1cd7931c09a7c70
kulikiliya <136320307+kulikiliya@users.noreply.github.com>
2023-08-23 12:09:39 +0300

    first_push

---
+++ src/cat-api.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_HbYY1KqMbjQNXatbIJfzQFpHx5Fg23kMUlHnsyE03TT8N7YOWY9RagC3Y5Jyf4Sf";
+  
+// const params = new URLSearchParams({
+//     limit: 5,
+//     has_breeds: 1,
+    
+// }).toString();
+
+function fetchBreeds() {
+   return axios.get('https://api.thecatapi.com/v1/breeds')
+       .then(response => {
+           const catsArray = response.data
+           console.log(catsArray)
+        return catsArray
+  })
+  .catch(error => {
+      console.error(error); 
+  });
+
+}
+
+
+ function fetchCatByBreed(breedId) {
+    return axios.get(`https://api.thecatapi.com/v1/images/search?breed_ids=d${breedId}`).then(response => {
+        console.log(response.data)
+        return response.data;
+    }).catch(error => {
+      throw console.error(error); 
+  });
+}
+
+export { fetchBreeds, fetchCatByBreed }
+
+// selector.addEventListener('change', event => {
+
+//     const selecorIdValue = event.currentTarget.value
+
+//     if (cachedCatsData[selecorIdValue]) {
+//         showCatImage(cachedCatsData[selecorIdValue])
+//     } else {
+       
+//         fetchCatByBreed(selecorIdValue)
+//             .then(showMyCat => {
+//                 cachedCatsData[selecorIdValue] = showMyCat;
+//                 showCatImage(showMyCat);
+//             });
+//     };
+// });
--- src/index.js
+++ src/index.js
@@ -1,2 +1,94 @@
-const test = 2;
-const dwdwa = 543;
+//live_HbYY1KqMbjQNXatbIJfzQFpHx5Fg23kMUlHnsyE03TT8N7YOWY9RagC3Y5Jyf4Sf
+
+import { fetchBreeds, fetchCatByBreed } from './cat-api'
+import SlimSelect from 'slim-select'
+import 'slim-select/dist/slimselect.css'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+
+
+const selector = document.querySelector('.breed-select');
+const waitMessage = document.querySelector('.loader');
+const errorMessage = document.querySelector('.error');
+const decr = document.querySelector('.cat-info')
+
+selector.addEventListener('change', searchForCatById)
+
+function createList() {
+
+    waitMessage.classList.remove('is-hidden');
+    selector.classList.add('is-hidden');
+    errorMessage.classList.add('is-hidden');
+
+fetchBreeds()
+    .then(catsArray => {
+
+    const catListRender = catsArray.map(({id, name}) => `<option value="${id}">${name}</option>`).join('');
+      
+    selector.innerHTML += catListRender;
+
+        new SlimSelect({
+            select: selector
+        });
+
+            waitMessage.classList.add('is-hidden');
+            selector.classList.remove('is-hidden')
+
+
+    }).catch(error => {
+        waitMessage.classList.add('is-hidden');
+        errorMessage.classList.remove('is-hidden');
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+  });
+
+}
+
+createList();
+
+
+function searchForCatById(event) {
+
+  waitMessage.classList.remove('is-hidden');
+    decr.classList.add('is-hidden')
+
+    const selecorIdValue = event.currentTarget.value
+
+    fetchCatByBreed(selecorIdValue).then(catData => {
+        showCatInfo(catData)
+
+
+        waitMessage.classList.add('is-hidden');
+            decr.classList.remove('is-hidden');
+
+
+    
+
+    }).catch(error => {
+     waitMessage.classList.add('is-hidden');
+        Notify.failure('Oops! Something went wrong! Try reloading the page!')
+        
+    });
+}
+
+
+function showCatInfo(catData) {
+    const { url } = catData[0]
+    const { name, temperament, description } = catData[0].breeds[0];
+    const catInfoString = `<div class='common-div'><img class="gallery__image" width = 500 src="${url}" alt="${name}" /> 
+    <div class='text-div'> 
+    <h2>${name}</h2>
+    <p class='temp-text'>${temperament}<p>
+    <p class='descr-text'>${description}</p>
+    </div>
+    </div>`;
+    
+  return decr.innerHTML = catInfoString;
+}
+
+Notify.init({
+  width: '400px',
+  position: 'left-top',
+  distance: '50px',
+    opacity: 1,
+  closeButton: true
+});
